b704a22694964ec4e20791175c7b8f8eaba2df6e
zakharikov <32512880+zakharikov@users.noreply.github.com>
2019-04-17 19:34:45 +0300

    тесты InMemoryAdminRestControllerSpringTest/InMemoryAdminRestControllerTest + jdbc тесты

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaUserServiceTest.java
@@ -1,10 +1,21 @@
 package ru.javawebinar.topjava.service.jpa;
 
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.repository.JpaUtil;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JPA;
 
 @ActiveProfiles(JPA)
 public class JpaUserServiceTest extends AbstractUserServiceTest {
+
+    @Autowired
+    protected JpaUtil jpaUtil;
+
+    @Before
+    public void clear2ndLevelCache() throws Exception {
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
 }
